515fa565d4f7a82943eba5dd1e20d43ac2bfb972
onyx-and-iris <code@onyxandiris.online>
2023-08-06 11:33:15 +0100

    use conditional assignment
    patch bump

--- lib/obsws/logger.rb
+++ lib/obsws/logger.rb
@@ -3,7 +3,7 @@ require "logger"
 module OBSWS
   module Logging
     def logger
-      @logger = Logger.new($stdout, level: ENV.fetch("OBSWS_LOG_LEVEL", "WARN"))
+      @logger ||= Logger.new($stdout, level: ENV.fetch("OBSWS_LOG_LEVEL", "WARN"))
       @logger.progname = instance_of?(::Module) ? name : self.class.name
       @logger
     end
